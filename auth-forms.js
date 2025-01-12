dd43953b2cad2035e6ccc5827bb982ad1b7ad81b
My shop <HAMDAOUIMOKRANE@GMAIL.COM>
2025-01-12 15:10:12 +0100

    Update auth-forms.js

--- auth-forms.js
+++ auth-forms.js
@@ -2,8 +2,3 @@ function showUserForm() {
     document.getElementById('userForm').style.display = 'block';
     document.getElementById('merchantFormSection').style.display = 'none';
 }
-
-function showMerchantForm() {
-    document.getElementById('merchantFormSection').style.display = 'block';
-    document.getElementById('userForm').style.display = 'none';
-}
